a99290710326d4709f5e0c1b7d725f8877f427e1
DiegoJfsr <diegojfsr@gmail.com>
2024-12-24 20:05:35 -0300

    ReactRouterComParametrosRota

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRoutes from './routes';
-import './index.css';
+// import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
